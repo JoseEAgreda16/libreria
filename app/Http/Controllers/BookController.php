2431404ef618b991f48452bb1c9e198dad3018f1
JoseEAgreda16 <joseonix.16@gmail.com>
2019-11-15 17:26:12 -0400

    Middleware Rol_id

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -18,16 +18,17 @@ class BookController extends Controller
     }
 
 
+    public function AddBooksView()
+    {
+        return view('bookregist');
+    }
+
     public function  AddBooks(Request $request)
     {
         $newuser = new Book($request->all());
         $newuser->save();
     }
 
-    public function AddBooksView()
-    {
-        return view('bookregist');
-    }
 
     public function UpdateBooks(Request $request, $title)
     {
--- app/Http/Middleware/VerifyRol.php
+++ app/Http/Middleware/VerifyRol.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Support\Facades\Auth;
+use App\User;
 use Closure;
 
-class VerifyRole
+class VerifyRol
 {
     /**
      * Handle an incoming request.
@@ -15,6 +17,12 @@ class VerifyRole
      */
     public function handle($request, Closure $next)
     {
+        $user = Auth::user();
+
+        if ($user->rol_id == 2) {
+            return redirect('/home');
+        }
+
         return $next($request);
     }
 }
