05b89830e8c1a2247ee10f5dc5e0ede365ba70f6
JoseEAgreda16 <joseonix.16@gmail.com>
2019-11-26 16:11:18 -0400

    Add SeedStatus and Request controller

--- app/Request.php
+++ app/Request.php
@@ -11,4 +11,19 @@ class Request extends Model
     protected $fillable = [
         'users_id', 'book_id', 'status_id', 'date'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo( User::class, 'users_id');
+    }
+
+    public function book()
+    {
+        return $this->belongsTo( Book::class,'book_id');
+    }
+
+    public function status()
+    {
+        return$this->belongsTo(Status::class,'status_id');
+    }
 }
