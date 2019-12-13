37e3393e2ce7bc0d881ad9cc58935dbba78c8dff
otniel acevedo <otniel.a.55@gmail.com>
2019-12-13 15:59:42 -0400

    solo faltan los botones

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,49 +25,5 @@ class HomeController extends Controller
         return view('index');
     }
 
-    public function home()
-    {
-        $orders = Orders::with(['user','inventory', 'status'])
-            ->get();
-        return view('orders.index')->with(['orders' => $orders]) ;
-    }
 
-    public function users()
-    {
-
-        $genders = Gender::all();
-        $authors = Author::all();
-
-        $user = Auth::user();
-
-        $orders = Orders::select('book_id')
-            ->where('users_id', $user->id)
-            ->where('status_id', '<>', 5)
-            ->pluck('book_id')->toArray();
-
-        $books = Book::select('books.*')
-            ->join('inventories', 'books.id', 'inventories.book_id')
-            ->where('inventories.status_id', 1)
-            ->whereNotIn('books.id', $orders)
-            ->distinct()
-            ->get();
-
-
-
-//        $books = Book::select('books.*')
-//            ->join('inventories', 'books.id', 'inventories.book_id')
-//            ->leftJoin('orders', function ($join) use ($user) {
-//                $join->on('orders.book_id', 'books.id')
-//                    ->where('orders.users_id', $user->id);
-//            })
-//            //->join('book_status', 'inventories.status_id', '=', 'book_status.id')
-//            ->whereNull('orders.id')
-//            ->distinct()
-//            ->with(['gender', 'author'])
-//            ->get();
-
-
-
-        return view('orders.bookrequest', ['books'=>$books,'genders' => $genders, 'authors' => $authors]);
-    }
 }
--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
 use App\Book;
+use App\Gender;
 use App\Inventory;
 use App\Orders;
 use Carbon\Carbon;
@@ -59,7 +61,10 @@ class OrdersController extends Controller
         return response('se dio);                                                                                                                                                                                           io',400);
     }
 
-    public function change(Request $request, $id)
+
+
+//gestion de administrador (rechazar o aceptar ordernes)
+    public function changeStatus(Request $request, $id)
     {
         $now = Carbon::now();
         $order = Orders::findOrFail($id);
@@ -68,28 +73,110 @@ class OrdersController extends Controller
         if ($request->input('status_id') == 2) {
 
             $order-> status_id = $request->input('status_id');
-            $order-> date_init = $now;
-            $order-> date_end = $now->addMonth();
+            $order-> date = $now;
 
             $order-> save();
 
-            return response('ok');
+            return response('ok, aprovado');
         }
 
         if ($request->input('status_id') == 3) {
 
         $order-> status_id = $request->input('status_id');
+        $order-> date = $now;
 
         $order-> save();
 
         Inventory::findOrFail($order->inventories_id)
              ->update(['status_id' => 1]);
 
-         return response('ok');
+         return response('ok, denegado ');
         }
 
-        return response('fail', 400);
+
+        if ($request->input('status_id') == 4) {
+
+            $order-> status_id = $request->input('status_id');
+            $order-> date_init = $now;
+
+            $order-> save();
+
+            return response('ok, en uso');
+        }
+
+
+        if ($request->input('status_id') == 5) {
+
+            $order->status_id = $request->input('status_id');
+            $order->date_end = $now;
+
+            $order->save();
+
+            Inventory::findOrFail($order->inventories_id)
+                ->update(['status_id' => 1]);
+
+            return response('ok');
+        }
+
+
+            return response('fail', 400);
 
     }
+
+
+
+
+    public function home()
+    {
+        $orders = Orders::with(['user','inventory', 'status'])
+            ->whereIn('status_id', [1, 2, 4])
+            ->get();
+        return view('orders.index')->with(['orders' => $orders]) ;
+    }
+
+
+
+
+    public function users()
+    {
+
+        $genders = Gender::all();
+        $authors = Author::all();
+
+        $user = Auth::user();
+
+        $orders = Orders::select('book_id')
+            ->where('users_id', $user->id)
+            ->whereNotIn('status_id', [5, 3, 6])
+            ->pluck('book_id')->toArray();
+
+        $books = Book::select('books.*')
+            ->join('inventories', 'books.id', 'inventories.book_id')
+            ->where('inventories.status_id', 1)
+            ->whereNotIn('books.id', $orders)
+            ->distinct()
+            ->get();
+
+        return view('orders.bookrequest', ['books'=>$books,'genders' => $genders, 'authors' => $authors]);
+    }
+
+
+
+    public function cancelOrders($id)
+    {
+        $order = Orders::findOrFail($id);
+
+
+        $order-> status_id = 6;
+
+        $order-> save();
+
+        Inventory::findOrFail($order->inventories_id)
+            ->update(['status_id' => 1]);
+
+        return response('ok');
+    }
+
+
 
 }
